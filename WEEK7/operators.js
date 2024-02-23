14b9a1d1852ccc17cfa884b9e7ed03a5309980ae
Joy Florence <144892578+joyflorence@users.noreply.github.com>
2024-02-23 18:21:06 +0300

    operators

--- WEEK7/operators.js
+++ WEEK7/operators.js
@@ -1,1 +1,18 @@
-//OPERATORS
+     //OPERATORS
+//Arithmetic Operators
+let a = 3
+let x = (100+6)*a
+console.log(x)
+let v = 5;
+x++;
+let z = v;
+console.log(z)
+
+//Assignment Operators
+
+//Comparison Operators
+//String Operators
+//Logical Operators
+//Bitwise Operators
+//Ternary Operators
+//Type Operators

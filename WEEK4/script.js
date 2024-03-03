709957531a734e71e55c0c2639839d1bfd23a767
Joy Florence <144892578+joyflorence@users.noreply.github.com>
2024-03-03 22:30:36 +0300

    Objects_In_Javascript

---
+++ WEEK4/script.js
@@ -0,0 +1,26 @@
+const mytext = document.getElementById("mytext");
+const mysubmit = document.getElementById("mysubmit");
+const resultelement = document.getElementById("resultelement");
+
+let myage;
+mysubmit.onclick = function(){
+    myage = mytext.value;
+    myage = Number(myage);
+    if(myage>=100){
+        resultelement.textContent = `Too old to access`;
+    }
+    else if(myage==0){ 
+        resultelement.textContent = `Cannot be zero`;
+    }
+    else if(myage >= 18){
+        resultelement.textContent = `Old enough`;
+    }
+    else if(myage<0){
+        resultelement.textContent = `Cannot be below zero`;
+    }
+    else{
+        resultelement.textContent = `Must be 18+`;
+    }
+}
+
+

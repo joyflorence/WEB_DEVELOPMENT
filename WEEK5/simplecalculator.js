d7c0e410da59dad61dfab809d21b11da234a0d0b
Joy Florence <144892578+joyflorence@users.noreply.github.com>
2024-03-05 21:51:04 +0300

    Update simplecalculator.js

--- WEEK5/simplecalculator.js
+++ WEEK5/simplecalculator.js
@@ -16,8 +16,3 @@ function calculate(expression) {
 // Test the calculate function with various expressions
 console.log(calculate('3 + 4'));    
 console.log(calculate('3 - 4'));    
-console.log(calculate('6*67'));        
-console.log(calculate('23 // 6'));       
-console.log(calculate('8 / 3'));        
-console.log(calculate('12 / 0'));       
-console.log(calculate("abc + 3"));     

e6574d2d910ef4342f9a43a26dd43e99419b4f79
Joy Florence <144892578+joyflorence@users.noreply.github.com>
2024-03-07 09:33:07 +0300

    Create functions.js

---
+++ WEEK8/functions.js
@@ -0,0 +1,8 @@
+function greetuser(Username){
+    return `hello ${Username}🥰`
+}
+console.log(greetuser('Joy'));
+
+//Anomymus functions
+const nums = (num1,num2)=>num1+num2;
+console.log(nums(67,5));
